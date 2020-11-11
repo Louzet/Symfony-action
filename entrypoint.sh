8afcc6beeba426b1490d99269c38a87fa613e44a
Louzet <micklouzet@hotmail.fr>
2020-11-11 19:10:20 +0100

    massive usage add_log method, handle error message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,45 @@
 #!/bin/sh -l
 
-## validate DATABASE_URL
-validate_database_url() {
-    env_file=$1
-
-    [ -z "$env_file" ] && echo "Missing .env or .env.dist file" && exit 1
-
-    grep DATABASE_URL= > DATABASE_URL < "$env_file"
-    [ -z "$DATABASE_URL" ] && echo "DATABASE_URL variable was not found" && exit 1
-}
-
 # Function to log result of a operation.
 add_log() {
+    
     gender=$1
     message=$2
 
-    printf "\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$gender": "$message"
+    if [ "$gender" = "info" ]; then
+
+        printf "\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$gender": "$message"
+
+    elif [ "$gender" = "error" ]; then
+
+        printf "\033[31;1m%s \033[0m\033[90;1m%s\033[0m\n" "$gender": "$message" && exit 1
+
+    fi
+}
+
+## validate DATABASE_URL
+validate_database_url() {
+
+    env_file=$1
+
+    [ -z "$env_file" ] && add_log "error" "Missing .env or .env.dist file" && exit 1
+
+    grep DATABASE_URL= > DATABASE_URL < "$env_file"
+
+    [ -z "$DATABASE_URL" ] && add_log "error" "DATABASE_URL variable was not found"
+
 }
 
 ## variables
 ENV_FILE=$1
 
-if [ -z "$ENV_FILE" ];
-then
-    echo "Missing .env or .env.dist file" && exit 1
+if [ -z "$ENV_FILE" ];then
+
+    add_log "error" "Missing .env or .env.dist file"
+
 else
-    add_log "info" "The $ENV_FILE was found !"
+    add_log "info" "The $ENV_FILE file successfully was found !"
+
     cat "$ENV_FILE";
 
     validate_database_url "$ENV_FILE"

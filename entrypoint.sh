a3261e25a1eb1f0c7f2a53c3d417656e59f66bcb
Louzet <micklouzet@hotmail.fr>
2020-11-11 18:13:33 +0100

    replace the methods above

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/sh -l
 
-ENV_FILE=$1
-
-if [ -z "$ENV_FILE" ];
-then
-    echo "Missing .env or .env.dist file" && exit 1
-else
-    add_log "info" "The $ENV_FILE was found !"
-    cat "$ENV_FILE";
-
-    validate_database_url "$ENV_FILE"
-fi
-## Create database step
-# search for an .env or .env.dist file
-# [ -z "$ENV_FILE" ] && echo "Missing .env or .env.dist file" && exit 1
-
-# grep DATABASE_URL= > DATABASE_URL < "$ENV_FILE"
-# echo "$DATABASE_URL";
-
 ## validate DATABASE_URL
 validate_database_url() {
     env_file=$1
@@ -35,3 +17,22 @@ add_log() {
 
     printf "\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$gender": "$message"
 }
+
+## variables
+ENV_FILE=$1
+
+if [ -z "$ENV_FILE" ];
+then
+    echo "Missing .env or .env.dist file" && exit 1
+else
+    add_log "info" "The $ENV_FILE was found !"
+    cat "$ENV_FILE";
+
+    validate_database_url "$ENV_FILE"
+fi
+## Create database step
+# search for an .env or .env.dist file
+# [ -z "$ENV_FILE" ] && echo "Missing .env or .env.dist file" && exit 1
+
+# grep DATABASE_URL= > DATABASE_URL < "$ENV_FILE"
+# echo "$DATABASE_URL";

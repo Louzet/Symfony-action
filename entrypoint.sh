e348bc0fc77727dfdb87ec81cb38c85d7097a3a4
Louzet <micklouzet@hotmail.fr>
2020-11-11 17:44:45 +0100

    deplace env_file into args instead of ENV

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -l
 
+ENV_FILE=$1
+
+cat "$ENV_FILE";
 ## Create database step
 # search for an .env or .env.dist file
-[ -z "$ENV_FILE" ] && echo "Missing .env or .env.dist file" && exit 1
+# [ -z "$ENV_FILE" ] && echo "Missing .env or .env.dist file" && exit 1
 
-grep DATABASE_URL= > DATABASE_URL < "$ENV_FILE"
-echo "$DATABASE_URL";
+# grep DATABASE_URL= > DATABASE_URL < "$ENV_FILE"
+# echo "$DATABASE_URL";
